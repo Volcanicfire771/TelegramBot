f168ba2bf1430b0e40ea6de72e4109b028932e75
Volcanicfire771 <marwanmoatassem@gmail.com>
2025-01-26 18:18:59 +0200

    Third Commit

--- functions/update.js
+++ functions/update.js
@@ -1,4 +1,28 @@
-exports.handler = async (event) => {
-    console.log("Received an update from Telegram!", event.body);
-    return { statusCode: 200 };
-  };
+const axios = require("axios");
+const MY_TOKEN = "7338554015:AAFdI_REkVhWC9OGLYHeNCWqAwJbbp-O-F4";
+const BASE_URL = `https://api.telegram.org/bot${MY_TOKEN}`;
+
+function getAxiosInstance(){
+    return{
+        get(method, params){
+            return axios.get(`/${method}`, {
+                baseURL: BASE_URL,
+                params,
+            });
+            
+        },
+
+
+        post(method, data){
+            return axios({
+                method: "post",
+                baseURL: BASE_URL,
+                url:`/${method}`,
+                data,
+            });
+            
+        },
+    };
+}
+
+module.exports = { axiosInstance: getAxiosInstance() };
