7789548ece9f03dffd4913e4be5492323ac16e4d
Volcanicfire771 <marwanmoatassem@gmail.com>
2025-01-26 18:26:43 +0200

    Test3

--- functions/update.js
+++ functions/update.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { handler } = require("../controller/library/index.js");
+const { handler } = require("../controller/library/index");
 const app = express();
 const port = 4040;
 
