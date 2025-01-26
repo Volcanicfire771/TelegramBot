73b980f7f397bb0e5b0b56fa5c2b8b4b653ec5b7
Volcanicfire771 <marwanmoatassem@gmail.com>
2025-01-26 18:54:54 +0200

    Test6

--- functions/update.js
+++ functions/update.js
@@ -1,7 +1,9 @@
 const sendMessage = require("../sendmessage");
+const handler = require("../controller/library/index");
+const handleMessage = require("../controller/library/telegram");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
-  await sendMessage(message.chat.id, "I got your message!");
+  await sendMessage(message.chat.id, handleMessage(event.body));
   return { statusCode: 200 };
 };
