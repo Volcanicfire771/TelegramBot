792024d366418c5408a2666799fdc4007bcc76d7
Volcanicfire771 <marwanmoatassem@gmail.com>
2025-01-26 19:18:48 +0200

    Test9

--- functions/update.js
+++ functions/update.js
@@ -3,6 +3,11 @@ const sendMessage = require("../sendmessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
+    if (message == "/start"){
+        await sendMessage(message.chat.id, "Ezayak");
+
+    }
+
   await sendMessage(message.chat.id, "I got your message!");
   return { statusCode: 200 };
 };
