c7e3fa37c22d7d8b1334ff5ae1238551277a3613
Volcanicfire771 <marwanmoatassem@gmail.com>
2025-01-26 18:45:09 +0200

    Test5

--- sendmessage.js
+++ sendmessage.js
@@ -1,3 +1,4 @@
+const axios = require("axios").default;
 module.exports = async (chat_id, text) => {
     await axios.post(`https://api.telegram.org/bot7338554015:AAFdI_REkVhWC9OGLYHeNCWqAwJbbp-O-F4/sendMessage`, {
       chat_id,
