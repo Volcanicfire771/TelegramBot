4496a9cc15dc1179d91045500360715507aff748
Volcanicfire771 <marwanmoatassem@gmail.com>
2025-01-26 19:21:39 +0200

    Test9

--- functions/update.js
+++ functions/update.js
@@ -3,11 +3,16 @@ const sendMessage = require("../sendmessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
-    if (message == "/start"){
+  console.log(message);
+  console.log(message.text);
+
+    if (message == "/start" || message.text == "/start"){
         await sendMessage(message.chat.id, "Ezayak");
 
     }
+else{
+    await sendMessage(message.chat.id, "I got your message!");
 
-  await sendMessage(message.chat.id, "I got your message!");
+}
   return { statusCode: 200 };
 };
